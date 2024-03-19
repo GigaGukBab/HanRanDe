e7e26c74954103bc7fdd53f6db5387a75305d83b
GigaGukBab <qew8502@gmail.com>
2024-03-19 21:39:55 +0900

    feat: added clock styling

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,13 @@
-const clock = document.querySelector("h2#clock");
+const clock_time = document.getElementsByClassName('clock_time')[0];
 
+// > 현재 시간을 가져와서 화면에 표시하고 <time> 태그의 datetime 속성을 업데이트하는 함수
 function getClock() {
   const date = new Date();
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  clock_time.innerText = `${hours}:${minutes}:${seconds}`;
+  clock_time.setAttribute('datetime', date.toISOString());
 }
 getClock(); // <- by calling getClock() initialy, we can see time immediately
 setInterval(getClock, 1000);
