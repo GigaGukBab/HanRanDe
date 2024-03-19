1e5cece67e99761fc84c9f85e47f343a82a207db
GigaGukBab <qew8502@gmail.com>
2024-03-20 00:19:50 +0900

    feat: added greeting container

--- js/greeting.js
+++ js/greeting.js
@@ -1,28 +1,32 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
-const greeting = document.querySelector("#greeting");
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
 
-const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY = "username";
+const greetingTitle = document.querySelector('#greetingTitle');
+const greetingSubTitle = document.querySelector('#greetingSubTitle');
+const greetingsContainer = document.querySelector('#greetingsContainer');
+
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings(username);
+  showGreetings(username);
 }
 
-function paintGreetings(username) {
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+function showGreetings(username) {
+  greetingTitle.innerText = `안녕하세요 ${username}님 !`;
+  greetingSubTitle.innerHTML = `<span class="Source">Source</span><span class="hub">Hub</span>에 방문해주셔서 감사합니다💋`;
+  greetingsContainer.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener("submit", onLoginSubmit);
+  loginForm.addEventListener('submit', onLoginSubmit);
 } else {
-  paintGreetings(savedUsername);
+  showGreetings(savedUsername);
 }
